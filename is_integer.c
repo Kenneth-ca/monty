ec3a57d981f47083c31e5327bd185ecf1dc55ba3
Kenneth-ca <kenneth.ca95@gmail.com>
2019-05-16 00:34:34 +0000

    [Feature] Function that check for an integer

---
+++ is_integer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _printf - prints according to format
+ * @format: The given format
+ *
+ * Return: 1 if it is a number, 0 if not.
+ */
+int is_integer(char *tokens)
+{
+	int i = 0;
+
+	if (tokens[i] == '-')
+		i++;
+	for (; tokens[i] != '\0'; i++)
+	{
+		if (tokens[i] < 48 || tokens[i] > 57)
+			return (0);
+	}
+return (1);
+}
